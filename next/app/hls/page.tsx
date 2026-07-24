d231b35bfaca7dd0fd46cfb6a69cab4287d5f803
John Sherrell <dev@ohn.sh>
2026-07-23 23:27:05 -0500

    New layout.module.css for scoped rules targeting the main layout (main, header, footer). Remove tailwind classes which made <body> a flex container.

--- next/app/hls/page.tsx
+++ next/app/hls/page.tsx
@@ -3,7 +3,6 @@ import HlsSwitch from './hls-switch'
 export default async function (props: PageProps<'/hls'>) {
   return (
     <main>
-      <h1 className="text-2xl font-bold">Dash</h1>
       <HlsSwitch className="mx-auto" />
     </main>
   )
--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import Script from 'next/script'
 import './globals.css'
+import css from './layout.module.css'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,14 +29,20 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      {/* No need to keep this here, but for the CDN experiment it's essential
+      {/* No need to keep this, but for the CDN experiment it's essential
       for it to be above the client-side navigation boundary. */}
-      <body className="min-h-full flex flex-col">
+      <body className={`${css.body}`}>
+        <header>
+          <h1 className="text-2xl font-bold">
+            <a href="/">Dash</a>
+          </h1>
+        </header>
         {children}
         <Script
           src="https://cdn.jsdelivr.net/npm/hls.js@latest"
           strategy="beforeInteractive"
         />
+        <footer>j@ohn.sh</footer>
       </body>
     </html>
   )
