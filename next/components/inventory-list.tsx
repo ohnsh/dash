1b9a772e1db705673464b74c69742cabe11c0dda
John Sherrell <dev@ohn.sh>
2026-08-08 06:00:23 -0500

    dash-next: InventoryList now queries inventories sorted by date with LIMIT 100, then organizes them by date so that the menu is hierarchical with dates at the top level, in reverse-chronological order.

--- next/components/inventory-list.tsx
+++ next/components/inventory-list.tsx
@@ -6,18 +6,56 @@ export default async function InventoryList() {
   // test fallback content
   // await new Promise((resolve) => setTimeout(resolve, 1000))
 
-  const rows = await dbQuery(`SELECT * FROM vod_index`)
+  const rows = await dbQuery(`
+    SELECT inventory_path, date
+    FROM vod_index
+    ORDER BY date DESC
+    LIMIT 100
+  `)
+
+  const collated = rows.reduce<Record<string, string[]>>((prev, current) => {
+    prev[current.date] ??= []
+    prev[current.date].push(current.inventory_path)
+    return prev
+  }, {})
 
   return (
     <ul>
-      {rows.map(({ inventory_path }) => {
-        const slug = r2PathToSlug(inventory_path)
-        return (
-          <li key={inventory_path}>
-            <Link href={`/${slug.join('/')}`}>{slugToTitle(slug)}</Link>
-          </li>
-        )
-      })}
+      {Object.entries(collated).map(([date, paths]) => (
+        <li key={date}>
+          <DayMenu date={date} inventories={paths} />
+        </li>
+      ))}
     </ul>
+  )
+}
+
+function DayMenu({
+  date,
+  inventories,
+}: {
+  date: string
+  inventories: string[]
+}) {
+  return (
+    <details>
+      <summary>
+        <Link href={date.replaceAll(/-/g, '/')}>{date}</Link>
+      </summary>
+      <ul>
+        {inventories.map((inventory) => (
+          <InventoryItem key={inventory} inventory={inventory} />
+        ))}
+      </ul>
+    </details>
+  )
+}
+
+function InventoryItem({ inventory }: { inventory: string }) {
+  const slug = r2PathToSlug(inventory)
+  return (
+    <li>
+      <Link href={`/${slug.join('/')}`}>{slugToTitle(slug)}</Link>
+    </li>
   )
 }
