c635f7714eadbcec2ce1b3d57950e0d8aac826b0
John Sherrell <dev@ohn.sh>
2026-08-14 23:24:18 -0500

    dash-next: root route now shows only video with speech, along with a (very rough) interface to skip to each speech segment.

--- next/components/vod-player.tsx
+++ next/components/vod-player.tsx
@@ -1,8 +1,11 @@
 'use client'
 
+import type { VoiceResult } from 'dash-vod/schema'
+import { getSpeechTotal } from 'dash-vod/util'
 import { use, useEffect, useRef } from 'react'
 import type { DashVideo } from '@/lib/dash-video'
-import { clientParamsToSrc } from '@/lib/vod-new'
+// src from pathname and searchparams
+// import { clientParamsToSrc } from '@/lib/vod-new'
 import css from './vod-player.module.css'
 
 export default function VodPlayer({
@@ -23,15 +26,49 @@ export default function VodPlayer({
     video.src = src
     // video.load()
 
+    if (!dv?.voiceSegments || !getSpeechTotal(dv)) return
+
+    const vtt = video.addTextTrack('chapters', 'speech', 'en')
+    const { segments } = dv.voiceSegments
+    const cues = segments.map(
+      (seg) => new VTTCue(seg.start, seg.end, '[speech]'),
+    )
+    for (const cue of cues) {
+      vtt.addCue(cue)
+    }
+    vtt.activeCues
+    vtt.mode = 'showing'
+
+    const timeupdateHandler = (_: Event) => {
+      let nextStart: number | undefined
+      for (const { start, end } of segments) {
+        if (video.currentTime < start) {
+          nextStart = start
+          break
+        }
+        if (video.currentTime < end) {
+          return
+        }
+      }
+      video.currentTime = nextStart ? nextStart : video.duration
+    }
+    // too much
+    // video.addEventListener('timeupdate', timeupdateHandler)
+
     return () => {
       // if (video.isConnected)
       video.pause()
       video.removeAttribute('src')
       video.load()
-    }
-  }, [src])
+      // there is no API to remove a text track once added.
+      vtt.mode = 'disabled'
 
-  // const vidUrl = v && `${slugToR2URL(slug)}/${v}`
+      // video.removeEventListener('timeupdate', timeupdateHandler)
+    }
+  }, [src, dv])
+
+  const width = dv?.meta_ffprobe.width ?? 1920
+  const height = dv?.meta_ffprobe.height ?? 1080
 
   // adding key={src} to <video> to unmount/remount it on src change.
   // still hearing phantom audio from previous sources, however.
@@ -43,12 +80,48 @@ export default function VodPlayer({
       {src && (
         <video
           ref={videoRef}
+          width={width}
+          height={height}
           crossOrigin="anonymous"
           autoPlay
           controls
           playsInline
+        >
+          <a href={src} download={dv?.name ?? 'video.mp4'}>
+            Download MP4
+          </a>
+        </video>
+      )}
+      {dv?.voiceSegments && (
+        <VoiceSegments
+          voiceSegments={dv.voiceSegments}
+          updatePosition={(pos: number) => {
+            if (videoRef.current) {
+              videoRef.current.currentTime = pos
+            }
+          }}
         />
       )}
     </div>
+  )
+}
+function VoiceSegments({
+  voiceSegments,
+  updatePosition,
+}: {
+  voiceSegments: VoiceResult
+  updatePosition: (pos: number) => void
+}) {
+  const { segments } = voiceSegments
+  return (
+    <ul>
+      {segments.map((seg) => (
+        <li key={seg.start}>
+          <button type="button" onClick={() => updatePosition(seg.start)}>
+            {seg.start}
+          </button>
+        </li>
+      ))}
+    </ul>
   )
 }
