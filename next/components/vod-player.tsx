179150180bf8803a8b0400894f904bb0bab82237
John Sherrell <dev@ohn.sh>
2026-08-17 17:07:38 -0500

    next: useCrop kind of works but it prevents native seek controls from working. The ultimate answer will be rendering custom controls on a <canvas> or <video> with srcObject fed from a canvas. Leaving out of VODPlayer for now.

--- next/components/vod-player.tsx
+++ next/components/vod-player.tsx
@@ -4,13 +4,11 @@ import type { VoiceResult } from '@dash/vod/schema'
 import { getSpeechTotal } from '@dash/vod/util'
 import { use, useEffect, useRef, useState } from 'react'
 import { type DashVideo, MIN_CONFIDENCE } from '@/lib/dash-video'
-import useCrop, { type Rect } from '@/lib/use-crop'
+// import useCrop from '@/lib/use-crop'
 import { timestampFromFilename, tsToString } from '@/lib/vod-new'
 // src from pathname and searchparams
 // import { clientParamsToSrc } from '@/lib/vod-new'
 import css from './vod-player.module.css'
-
-const CROP_RECT: Rect = { x: 0, y: 0, width: 1920, height: 1080 }
 
 export default function VODPlayer({
   src,
@@ -22,13 +20,8 @@ export default function VODPlayer({
   const dv = videoPromise && use(videoPromise)
   const videoRef = useRef<HTMLVideoElement>(null)
   const [activeIndex, setActiveIndex] = useState<number | undefined>(undefined)
-  const [cropRect, setCropRect] = useState<Rect | undefined>(undefined)
   src ??= dv?.src
-  useCrop(videoRef, src, cropRect)
-
-  const toggleCrop = () => {
-    setCropRect((prev) => (prev ? undefined : CROP_RECT))
-  }
+  // useCrop(src, videoRef)
 
   useEffect(() => {
     const $video = videoRef.current
@@ -103,7 +96,7 @@ export default function VODPlayer({
             width={width}
             height={height}
             crossOrigin="anonymous"
-            autoPlay
+            // autoPlay
             controls
             playsInline
           >
@@ -113,12 +106,7 @@ export default function VODPlayer({
           </video>
         )}
       </div>
-      <div className={css.vidFooter}>
-        {dv && <Timestamp dashVideo={dv} />}
-        <button type="button" onClick={toggleCrop}>
-          crop
-        </button>
-      </div>
+      <div className={css.vidFooter}>{dv && <Timestamp dashVideo={dv} />}</div>
       {dv?.voiceSegments && (
         <VoiceSegments
           voiceSegments={dv.voiceSegments}
--- next/lib/use-crop.ts
+++ next/lib/use-crop.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export interface Rect {
   x: number
@@ -7,20 +7,49 @@ export interface Rect {
   height: number
 }
 
+const CROPS = {
+  topLeft: {
+    x: 0,
+    y: 0,
+    width: 1920,
+    height: 1080,
+  },
+  bottomLeft: {
+    x: 0,
+    y: 1080,
+    width: 1920,
+    height: 1080,
+  },
+  topRight: {
+    x: 1920,
+    y: 0,
+    width: 1920,
+    height: 1080,
+  },
+  bottomRight: {
+    x: 1920,
+    y: 1080,
+    width: 1920,
+    height: 1080,
+  },
+}
+
 export default function useCrop(
-  videoRef: React.RefObject<HTMLVideoElement | null>,
   src: string | undefined,
-  rect: Rect | undefined,
+  userVideoRef = useRef<HTMLVideoElement>(null),
+  enabled = true,
 ) {
   const srcVideoRef = useRef<HTMLVideoElement>(null)
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const ctxRef = useRef<CanvasRenderingContext2D>(null)
+  const [crop, setCrop] = useState<Rect | undefined>(undefined)
 
   useEffect(() => {
-    if (!src || !rect || !videoRef.current) return
+    if (!src || !enabled || !userVideoRef.current) return
 
     // const { videoWidth, videoHeight } = videoRef.current
 
+    const userVideo = userVideoRef.current
     const srcVideo = document.createElement('video')
     const canvas = document.createElement('canvas')
     const ctx = canvas.getContext('2d', { willReadFrequently: true })
@@ -32,43 +61,93 @@ export default function useCrop(
     canvasRef.current = canvas
     ctxRef.current = ctx
 
-    const { x, y, width, height } = rect
-    const srcRect = [x, y, width, height] as const
-    const destRect = [0, 0, width, height] as const
-    canvas.width = width
-    canvas.height = height
-
     srcVideo.crossOrigin = 'anonymous'
-    // srcVideo.autoplay = true
     srcVideo.src = src
 
-    // srcVideo.hidden = true
-    // document.body.appendChild(srcVideo)
-    if (!videoRef.current) return
-    videoRef.current.srcObject = canvas.captureStream(30)
-
-    videoRef.current.addEventListener('play', (_e) => {
-      srcVideo.play()
-    })
-    videoRef.current.addEventListener('pause', (_e) => {
-      srcVideo.pause()
-    })
-
-    let handle = srcVideo.requestVideoFrameCallback(renderFrame)
-    function renderFrame() {
-      if (!ctx) return
-      ctx.drawImage(srcVideo, ...srcRect, ...destRect)
-      // if (srcVideo.paused || srcVideo.ended) return
-      handle = srcVideo.requestVideoFrameCallback(renderFrame)
-    }
-
     return () => {
-      srcVideo.cancelVideoFrameCallback(handle)
       srcVideoRef.current = null
       canvasRef.current = null
       ctxRef.current = null
-      if (!videoRef.current) return
-      videoRef.current.srcObject = null
+
+      userVideo.srcObject = null
     }
-  }, [rect, src, videoRef.current])
+  }, [enabled, src])
+
+  useEffect(() => {
+    const srcVideo = srcVideoRef.current
+    const userVideo = userVideoRef.current
+    const canvas = canvasRef.current
+    const ctx = ctxRef.current
+
+    if (!srcVideo || !userVideo || !canvas || !ctx) {
+      return
+    }
+
+    const playHandler = () => {
+      srcVideo.play()
+    }
+    const pauseHandler = () => {
+      srcVideo.pause()
+    }
+
+    let cbHandle: number | undefined
+    const clickHandler = (ev: MouseEvent) => {
+      if (!ev.altKey) {
+        return
+      }
+      ev.preventDefault()
+
+      if (crop) {
+        if (typeof cbHandle === 'number') {
+          srcVideo.cancelVideoFrameCallback(cbHandle)
+          cbHandle = undefined
+        }
+        userVideo.srcObject = null
+        srcVideo.pause()
+        userVideo.play()
+        setCrop(undefined)
+      } else {
+        // get quadrant
+        // ev.offsetX
+        // ev.offsetY
+        const { x, y, width, height } = CROPS.bottomLeft
+        const srcRect = [x, y, width, height] as const
+        const destRect = [0, 0, width, height] as const
+
+        canvas.width = width
+        canvas.height = height
+
+        userVideo.srcObject = canvas.captureStream(30)
+
+        cbHandle = srcVideo.requestVideoFrameCallback(renderFrame)
+        srcVideo.play()
+        userVideo.play()
+
+        function renderFrame() {
+          if (!ctx || !srcVideo) return
+          ctx.drawImage(srcVideo, ...srcRect, ...destRect)
+          // if (srcVideo.paused || srcVideo.ended) return
+          cbHandle = srcVideo.requestVideoFrameCallback(renderFrame)
+        }
+        setCrop(CROPS.bottomLeft)
+      }
+    }
+
+    userVideo.addEventListener('play', playHandler)
+    userVideo.addEventListener('pause', pauseHandler)
+    userVideo.addEventListener('click', clickHandler)
+
+    return () => {
+      if (typeof cbHandle === 'number') {
+        // srcVideo.cancelVideoFrameCallback(cbHandle)
+        // cbHandle = undefined
+      }
+
+      userVideo.removeEventListener('play', playHandler)
+      userVideo.removeEventListener('pause', pauseHandler)
+      userVideo.removeEventListener('click', clickHandler)
+    }
+  }, [crop])
+
+  return { videoRef: userVideoRef }
 }
