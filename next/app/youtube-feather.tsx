fd71b98ad61649953ce6c327e92e51e4f81e2f76
John Sherrell <dev@ohn.sh>
2026-08-25 21:50:43 -0500

    next: social icon accessibility tweaks. Descriptive text now in a[aria-label], with the generic icon description in svg > title (ignored inside of aforementioned links).

--- next/app/youtube-feather.tsx
+++ next/app/youtube-feather.tsx
@@ -12,10 +12,10 @@ export default function YoutubeFeather(props: SVGProps<SVGSVGElement>) {
       strokeLinecap="round"
       strokeLinejoin="round"
       role="img"
-      aria-label="YouTube"
       className={`${css.container} ${props.className ? props.className : ''}`}
       {...props}
     >
+      <title>YouTube icon</title>
       <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2a29 29 0 0 0 .46-5.25a29 29 0 0 0-.46-5.33" />
       <path d="m9.75 15.02l5.75-3.27l-5.75-3.27z" />
     </svg>
